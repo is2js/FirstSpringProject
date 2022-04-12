33dfc985a22809daa48ae926f2924bb8ca562892
is2js <tingstyle1@gmail.com>
2022-04-12 22:59:25 +0900

    3- mvc 복습

--- docs/readme.md
+++ docs/readme.md
@@ -22,6 +22,8 @@ start.spring.io
     1. 여기선 mustache가 뷰 템플릿 엔진으로서 기능을 제공해준다.
         1. template폴더에 .mustache를 생성하는데, 인식을 못하니 플러그인부 설치치
     2. **view template에는 2가지 동료가 있으며, 화면을 볼려면 2가지 동료를 만들어줘야한다.**
-       1. 처리:controller
-       2. data:model
-3. 
+        1. 처리:controller
+        2. data:model
+
+3. controller는 client로부터 요청을 받는 역할이다. view는 최종페이지를 만들고, 거기에 쓰일데이터들을 model이 만들어준다.
+    1. **해석 및 구현 순서: client -> controller 메서드 및 @애너테이션으로 접속요청 + model까지 받아서 -> view 템플릿 페이지에 + 변수값으로 전송 
--- src/main/java/com/example/firstproject/controller/FirstController.java
+++ src/main/java/com/example/firstproject/controller/FirstController.java
@@ -4,25 +4,26 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
-//2-1. view의 동료임을 선언한다.
 @Controller
 public class FirstController {
 
-    //2-3. 동료내부에서 해당 페이지로 @접속할 url도 선언해줘야한다.
-    // 서버 재시작후 http://localhost:8080/hi
     @GetMapping("/hi")
-
-    //2-5. view의 또다른 동료는 controller의 파라미터로 받아야한다.
-//    public String niceToMeetYou() {
-    // -> import class시 org.spring~을 찾아서 해주면 된다
     public String niceToMeetYou(final Model model) {
-        //2-6. 이제 view로 보내줄 변수를 model에 등록해줄 수 있다.
         model.addAttribute("username", "hongpark");
 
-        //return "";
-        //2-2. controller 동료는 메소드를 통해 -> view 템플릿 페이지를 반환할 수 있게 해줘야한다.
-        //    확장자만 빼고, 페이지명만 string으로 넘겨준다.
-        return "greetings"; // templates/greetings.mustache를 알아서 찾아서 브라우저로 전송해준다.
-        // -> 엥? http://localhost:8080/greetings.mustache로 들어가도 안보인다. -> 빠진게 있다.
+        return "greetings"; // templates/greetings.mustache -> 브라우저로 전송
+    }
+
+    //3-1. 새로운 요청을 받을 수 있도록 라우트를 작성해준다.
+    // - 요청을 받는 것은 controller니, 새로운 컨트롤러를 받아도 좋다.
+    // - 뷰템플릿을 사용한다면, 파일명을 응답값으로 적어줘야하므로 public String은 고정이다.
+    @GetMapping("/bye")
+    //3-4.
+//    public String seeYouNext() {
+    public String seeYouNext(final Model model) {
+        model.addAttribute("nickname", "홍길동");
+
+        //3-2. 템플릿의 파일명만 적어주고, 만들어주러 가자.
+        return "goodbye";
     }
 }
